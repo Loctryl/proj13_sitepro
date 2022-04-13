adb3e90c8ec9cba159a4d0519261300047411f78
Karma <92379424+Karma192@users.noreply.github.com>
2022-04-13 12:05:11 +0200

    navbar

--- src/components/menu/navbar/navbar.js
+++ src/components/menu/navbar/navbar.js
@@ -2,14 +2,15 @@ import { Nav, Navbar, Stack } from "react-bootstrap";
 import "./navbar.css";
 
 export default function NavBar() {
-    return <div class="navBar">
-        <Navbar>
-            <Stack class="border" direction="horizontal" gap={5}>
+    return <div className="marginNav">
+        <Navbar className="SetNavBar">
+            <Stack direction="horizontal" gap={5}>
                 <Nav.Link href="#home">actus</Nav.Link>
                 <Nav.Link href="#features">about us</Nav.Link>
                 <Nav.Link href="#pricing">teams</Nav.Link>
                 <Nav.Link href="#pricing">shop</Nav.Link>
             </Stack>
         </Navbar>
+        <div className="line"></div>
     </div>
 }
