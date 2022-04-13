f1d8dc2d31e751146c75ccac039d0c60c22500ff
Thomas <tdelorme@gaming.tech>
2022-04-13 12:04:45 +0200

    footer start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from './logo.svg';
-import {React, Component} from 'react';
+import { React, Component } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/menu/navbar/navbar';
+import Footer from './components/menu/footer/footer.js'
+import NavBar from './components/menu/navbar/navbar.js'
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      news : [],
-      partners : [],
-      players : [],
-      sponsors : [],
-      teams : []
+      news: [],
+      partners: [],
+      players: [],
+      sponsors: [],
+      teams: []
     }
   }
 
@@ -21,45 +22,33 @@ class App extends Component {
     let datas = null
     let reponse = null
 
-    reponse = await fetch('http://localhost:1337/api/news',{method:'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
+    reponse = await fetch('http://localhost:1337/api/news', { method: 'GET', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } })
     datas = await reponse.json()
-    this.setState({news:datas, loading:false})
+    this.setState({ news: datas, loading: false })
 
-    reponse = await fetch('http://localhost:1337/api/partners',{method:'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
+    reponse = await fetch('http://localhost:1337/api/partners', { method: 'GET', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } })
     datas = await reponse.json()
-    this.setState({partners:datas, loading:false})
+    this.setState({ partners: datas, loading: false })
 
-    reponse = await fetch('http://localhost:1337/api/players',{method:'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
+    reponse = await fetch('http://localhost:1337/api/players', { method: 'GET', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } })
     datas = await reponse.json()
-    this.setState({players:datas, loading:false})
+    this.setState({ players: datas, loading: false })
 
-    reponse = await fetch('http://localhost:1337/api/sponsors',{method:'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
+    reponse = await fetch('http://localhost:1337/api/sponsors', { method: 'GET', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } })
     datas = await reponse.json()
-    this.setState({sponsors:datas, loading:false})
+    this.setState({ sponsors: datas, loading: false })
 
-    reponse = await fetch('http://localhost:1337/api/teams',{method:'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
+    reponse = await fetch('http://localhost:1337/api/teams', { method: 'GET', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } })
     datas = await reponse.json()
-    this.setState({teams:datas, loading:false})
+    this.setState({ teams: datas, loading: false })
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <NavBar/>
+        <Footer/>
+      </>
     );
   }
 }
---
+++ src/components/menu/footer/footer.js
@@ -0,0 +1,10 @@
+import { Col, Container, Row } from "react-bootstrap";
+import './footer.css';
+
+export default function Footer() {
+  return (
+      <footer className="page-footer font-small footer">
+        <h3>footer</h3>
+      </footer>
+  );
+}
