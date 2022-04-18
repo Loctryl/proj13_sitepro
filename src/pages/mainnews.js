87cadde6105aceab83fad40e90105a7b7d225338
Loctryl <letallec.thibautl@gmail.com>
2022-04-18 17:18:51 +0200

    Update

--- src/pages/mainnews.js
+++ src/pages/mainnews.js
@@ -1,13 +1,26 @@
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Col, Image, Pagination } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Footer from "../components/menu/footer";
 import NavBar from "../components/menu/navbar";
-import Footer from '../components/menu/footer.js'
 import TilesNews from "../components/tiles/tilesnews";
 import "./style/mainnews.css";
 
 
 function MainNews (props) {
+  let length = props.news.data.length / 4
+  if(props.news.data.lenght%4 !== 0){
+    length += 1
+  }
+
+  let items = [];
+  for (let number = 1; number <= length; number++) {
+    items.push(
+      <Pagination.Item key={number} active={number === 1}>
+        {number}
+      </Pagination.Item>,
+    );
+  }
+
   return (
     <>
       <NavBar />
@@ -17,10 +30,14 @@ function MainNews (props) {
           {props.news.data && props.news.data.map((news, i)=>(
             <Col sm={12} md={{ span: 4, offset: 1 }} lg={{ span: 6, offset: 0 }}>
               <Link to={"/mainnews/news?id="+i}>
-                <TilesNews news={news}/></Link>
+                <TilesNews news={news}/>
+              </Link>
             </Col>
           ))}
         </Row>
+      </div>
+      <div className="pagination-news">
+        <Pagination>{items}</Pagination>
       </div>
       <Footer/>
     </>
