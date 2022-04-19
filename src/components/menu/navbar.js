08179ec95363a854c5e756ed16b4c6c2c180fe7c
Karma <agrandpaul@gaming.tech>
2022-04-19 12:52:45 +0200

    navbar fix

--- src/components/menu/navbar.js
+++ src/components/menu/navbar.js
@@ -5,14 +5,21 @@ import "./navbar.css";
 export default function NavBar() {
   return (
     <div className="marginNav">
-      <Link to="/">
+      {/* <Link to={"/"}>
         <img
           className="logoNavbar"
           src={"../../../database/public/uploads/Logo_Colored_PNG_529f0a5229.png"}
           alt="ALKYA's Logo"
         />
-      </Link>
+      </Link> */}
       <Navbar className="SetNavBar">
+        <Link to={"/"}>
+          <img
+            className="logoNavbar"
+            src={"../../../database/public/uploads/Logo_Colored_PNG_529f0a5229.png"}
+            alt="ALKYA's Logo"
+          />
+        </Link>
         <Stack direction="horizontal" gap={5}>
           <Link to="/mainnews">actus</Link>
           <Link to="/aboutus">about us</Link>
