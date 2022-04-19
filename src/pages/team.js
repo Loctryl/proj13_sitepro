acdbdb728162d3644cdebce962b1bad74779fad8
Karma <agrandpaul@gaming.tech>
2022-04-19 12:29:01 +0200

    team pages set up

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,16 +1,45 @@
+import { Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Footer from "../components/menu/footer";
 import NavBar from "../components/menu/navbar";
 import "./style/team.css";
 
-function Team(props){
+function Team(props) {
 
-    return (
-      <>
+  return (
+    <>
       <NavBar />
-      <p>oui</p>
-      <Footer/>
-      </>
-    )
-  }
-  
-  export default Team;
+      <div className="teamBanner"></div>
+      <h2 className="line-up">notre line-up</h2>
+      <h3 className="teamH3">joueurs</h3>
+      <Row>
+        <Col sm={6} md={4} lg={4} className="playerCard">
+          <Link to={"/mainteam/team/player"}>
+            <img src="../../database/public/uploads/rekkles.jpeg" alt="Alkya's players images" className="imgCard"></img>
+            <div>
+              <p>prénom</p>
+              <p className="blueOne">"pseudo"</p>
+              <p>nom de famille</p>
+            </div>
+          </Link>
+        </Col>
+      </Row>
+      <h3 className="teamH3">coach</h3>
+      <Row>
+        <Col sm={6} md={4} lg={4} className="playerCard">
+          <Link to={"/mainteam/team/player"}>
+            <img src="../../database/public/uploads/rekkles.jpeg" alt="Alkya's players images" className="imgCard"></img>
+            <div>
+              <p>prénom</p>
+              <p className="blueOne">"pseudo"</p>
+              <p>nom de famille</p>
+            </div>
+          </Link>
+        </Col>
+      </Row>
+      <Footer />
+    </>
+  )
+}
+
+export default Team;
