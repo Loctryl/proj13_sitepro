f015728fd1f2b01c5c173abeaa16e324d9e85b3c
Thomas <tdelorme@gaming.tech>
2022-04-19 11:59:55 +0200

    caroussel home

--- src/components/menu/navbar.js
+++ src/components/menu/navbar.js
@@ -8,7 +8,7 @@ export default function NavBar() {
       <Link to="/">
         <img
           className="logoNavbar"
-          src={"http://localhost:1337/uploads/Logo_Colored_PNG_3d29f3295a.png?updated_at=2022-04-12T08:59:34.451Z"}
+          src={"http://localhost:1337/uploads/Logo_Colored_PNG_529f0a5229.png?updated_at=2022-04-12T08:59:34.540Z"}
           alt="ALKYA's Logo"
         />
       </Link>
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,26 @@ import NavBar from '../components/menu/navbar.js';
 import TilesStaff from "../components/tiles/tilesstaff";
 
 function Home(props) {
+
+  const games = {
+    RocketLeague: { 
+      link: "http://localhost:1337/uploads/rocketleague_cb091ec5e2.jpg?updated_at=2022-04-19T09:41:41.612Z",
+      alt: "Valorant image for Alkya"
+    },
+    LeagueOfLegend: {
+      link: "http://localhost:1337/uploads/lol_6a042d98a5.jpg?updated_at=2022-04-19T09:40:52.841Z",
+      alt: "League Of Legend image for Alkya"
+    },
+    RainbowSix: {
+      link: "http://localhost:1337/uploads/rainbowsiege_997cfd11f4.jpg?updated_at=2022-04-19T09:40:57.873Z",
+      alt: "RainbowSix image for Alkya"
+    },
+    Valorant: {
+      link: "http://localhost:1337/uploads/Valorant_381ee9b450.png?updated_at=2022-04-19T09:40:47.315Z",
+      alt: "Valorant image for Alkya"
+    }
+  }
+
   return (
     <>
       <NavBar />
@@ -15,46 +35,19 @@ function Home(props) {
       </Col></Row>
       <TilesStaff staff={props.staff} />
       <br />
-      <Row>
-        <Col xs={{ span: 2, offset: 1 }} md={{ span: 2, offset: 1 }}>
-          <img src="/equipeacc.png" className='img-fluid rounded-circle' alt="" /></Col>
-        <Col xs={{ span: 2, offset: 1 }} md={{ span: 2, offset: 1 }}>
-          <img src="/equipeacc.png" className='img-fluid rounded-circle' alt="" /></Col>
-        <Col xs={{ span: 2, offset: 1 }} md={{ span: 2, offset: 1 }}>
-          <img src="/equipeacc.png" className='img-fluid rounded-circle' alt="" /></Col>
-        <Col xs={{ span: 2, offset: 1 }} md={{ span: 2, offset: 1 }}>
-          <img src="/equipeacc.png" className='img-fluid rounded-circle' alt="" /></Col>
-      </Row>
-      <br />
       <Row><Col xs={{ span: 10, offset: 1 }} md={{ span: 10, offset: 1 }}>
         <Carousel interval={null} fade={true}>
           <Carousel.Item>
-            <img
-              className="d-block w-100 img-fluid  customImg"
-              src="/Valorant.png"
-              alt="First slide"
-            />
+            <img className="d-block w-100 img-fluid customImg" src={games.Valorant.link} alt={games.Valorant.alt} />
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100 h-100 img-fluid  customImg"
-              src="/lol.jpg"
-              alt="Second slide"
-            />
+            <img className="d-block w-100 h-100 img-fluid customImg" src={games.LeagueOfLegend.link} alt={games.LeagueOfLegend.alt} />
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100 img-fluid  customImg"
-              src="/rocketleague.jpg"
-              alt="Third slide"
-            />
+            <img className="d-block w-100 img-fluid customImg" src={games.RocketLeague.link} alt={games.RocketLeague.alt} />
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100 img-fluid  customImg"
-              src="/rainbowsiege.jpg"
-              alt="Third slide"
-            />
+            <img className="d-block w-100 img-fluid  customImg" src={games.RainbowSix.link} alt={games.RainbowSix.alt} />
           </Carousel.Item>
         </Carousel>
       </Col>
