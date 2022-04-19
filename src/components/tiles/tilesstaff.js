9eb5501fce3e6e527f2ac878977c1f999b4ec1fa
Thomas <tdelorme@gaming.tech>
2022-04-19 12:34:20 +0200

    caroussel home v2

--- src/components/tiles/tilesstaff.js
+++ src/components/tiles/tilesstaff.js
@@ -5,23 +5,15 @@ export default function TilesStaff(props) {
 
   return (
     <>
-      <h2>staff</h2>
-      <Container className="marginStaff">
-        <Row>
-          {props.staff.data?.map((staff, i) => (
-            <Col key={i} xs={3} md={3} lg={3}>
-              <Card className="imageStaff">
-                <Card.Img
-                  src={"http://localhost:1337" + staff.attributes.image.data.attributes.url}
-                  alt={staff.attributes.name + ": Alkya staff"}
-                  className="cardImg"
-                />
-              </Card>
-              <p>{staff.attributes.first_name} {staff.attributes.last_name} - {staff.attributes.post}</p> 
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <Card className="imageStaff">
+        <Card.Img
+          src={"http://localhost:1337" + props.staff.attributes.image.data.attributes.url}
+          alt={props.staff.attributes.name + ": Alkya staff"}
+          className="cardImg"
+        />
+      </Card>
+      <p>{props.staff.attributes.first_name} {props.staff.attributes.last_name} - {props.staff.attributes.post}</p> 
     </>
+      
   )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -8,52 +8,71 @@ import TilesStaff from "../components/tiles/tilesstaff";
 function Home(props) {
 
   const games = {
-    RocketLeague: { 
-      link: "http://localhost:1337/uploads/rocketleague_cb091ec5e2.jpg?updated_at=2022-04-19T09:41:41.612Z",
-      alt: "Valorant image for Alkya"
+    0 : {
+      RocketLeague: { 
+        link: "http://localhost:1337/uploads/rocketleague_cb091ec5e2.jpg?updated_at=2022-04-19T09:41:41.612Z",
+        alt: "Valorant image for Alkya"
+      }
     },
-    LeagueOfLegend: {
-      link: "http://localhost:1337/uploads/lol_6a042d98a5.jpg?updated_at=2022-04-19T09:40:52.841Z",
-      alt: "League Of Legend image for Alkya"
+    1 : {
+      LeagueOfLegend: {
+        link: "http://localhost:1337/uploads/lol_6a042d98a5.jpg?updated_at=2022-04-19T09:40:52.841Z",
+        alt: "League Of Legend image for Alkya"
+      }
     },
-    RainbowSix: {
-      link: "http://localhost:1337/uploads/rainbowsiege_997cfd11f4.jpg?updated_at=2022-04-19T09:40:57.873Z",
-      alt: "RainbowSix image for Alkya"
+    2 : {
+      RainbowSix: {
+        link: "http://localhost:1337/uploads/rainbowsiege_997cfd11f4.jpg?updated_at=2022-04-19T09:40:57.873Z",
+        alt: "RainbowSix image for Alkya"
+      }
     },
-    Valorant: {
-      link: "http://localhost:1337/uploads/Valorant_381ee9b450.png?updated_at=2022-04-19T09:40:47.315Z",
-      alt: "Valorant image for Alkya"
+    3 : {
+      Valorant: {
+        link: "http://localhost:1337/uploads/Valorant_381ee9b450.png?updated_at=2022-04-19T09:40:47.315Z",
+        alt: "Valorant image for Alkya"
+      }
     }
   }
 
   return (
     <>
       <NavBar />
-      <Row><Col xs={12} md={12}><h1>ALKYA</h1></Col></Row>
-      <Row><Col xs={{ span: 10, offset: 1 }} md={{ span: 10, offset: 1 }}>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-      </Col></Row>
-      <TilesStaff staff={props.staff} />
-      <br />
-      <Row><Col xs={{ span: 10, offset: 1 }} md={{ span: 10, offset: 1 }}>
-        <Carousel interval={null} fade={true}>
-          <Carousel.Item>
-            <img className="d-block w-100 img-fluid customImg" src={games.Valorant.link} alt={games.Valorant.alt} />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100 h-100 img-fluid customImg" src={games.LeagueOfLegend.link} alt={games.LeagueOfLegend.alt} />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100 img-fluid customImg" src={games.RocketLeague.link} alt={games.RocketLeague.alt} />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100 img-fluid  customImg" src={games.RainbowSix.link} alt={games.RainbowSix.alt} />
-          </Carousel.Item>
-        </Carousel>
-      </Col>
-    </Row>
-    <Footer/>
-  </>
+
+      <Row>
+        <Col xs={12} md={12}>
+          <h1>ALKYA</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={{ span: 10, offset: 1 }} md={{ span: 10, offset: 1 }}>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+      </Row>
+
+      <h2>staff</h2>
+      <Row>
+        {props.staff.data?.map((staff, i) => (
+          <Col xs={3} md={3} lg={3}>
+            <TilesStaff key={i} staff={staff} />
+          </Col>
+        ))}
+      </Row>
+
+      <Row>
+        <Col xs={{ span: 10, offset: 1 }} md={{ span: 10, offset: 1 }}>
+          <Carousel interval={null} fade={true}>
+            {console.log(games)}
+            {games.map((games, i) => (
+              <Carousel.Item key={i}>
+                <img className="d-block w-100 img-fluid customImg" src={games.link} alt={games.alt} />
+            </Carousel.Item>
+            ))}
+          </Carousel>
+        </Col>
+      </Row>
+    
+      <Footer/>
+    </>
   );
 }
 export default Home;
