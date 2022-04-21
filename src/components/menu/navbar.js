df46d4a9411cfe49efcaae89958f9f190603f84b
Karma <agrandpaul@gaming.tech>
2022-04-21 10:13:26 +0200

    navbar responsive

--- src/components/menu/navbar.js
+++ src/components/menu/navbar.js
@@ -1,26 +1,62 @@
-import { Navbar, Stack } from "react-bootstrap";
+import { Offcanvas } from "bootstrap";
+import { Container, Nav, Navbar, NavDropdown, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export default function NavBar() {
   return (
-    <div className="marginNav">
-      <Navbar className="SetNavBar">
-        <Link to={"/"}>
-          <img
-            className="logoNavbar"
-            src={"http://localhost:1337/uploads/Logo_Colored_PNG_529f0a5229.png?updated_at=2022-04-12T08:59:34.540Z"}
-            alt="ALKYA's Logo"
-          />
-        </Link>
-        <Stack direction="horizontal" gap={5}>
-          <Link to="/mainnews">actus</Link>
-          <Link to="/aboutus">about us</Link>
-          <Link to="/mainteam">teams</Link>
-          <Link to="/shop">shop</Link>
-        </Stack>
-      </Navbar>
-      <div className="line" />
-    </div>
+    <>
+      <div className="marginNav element-to-hide-on-small">
+        <Navbar className="SetNavBar">
+          <Link to="/">
+            <img
+              className="logoNavbar"
+              src={"http://localhost:1337/uploads/Logo_Colored_PNG_529f0a5229.png?updated_at=2022-04-12T08:59:34.540Z"}
+              alt="ALKYA's Logo"
+            />
+          </Link>
+          <Stack direction="horizontal" gap={5}>
+            <Link to="/mainnews">actus</Link>
+            <Link to="/aboutus">about us</Link>
+            <Link to="/mainteam">teams</Link>
+            <Link to="/shop">shop</Link>
+          </Stack>
+        </Navbar>
+        <div className="line" />
+      </div>
+      <div className="element-to-hide-on-other">
+        <Navbar bg="light" expand={false}>
+          <Container fluid>
+            <Navbar.Brand>
+              <Link to="/">
+                <img
+                  className="logoNavbar"
+                  src={"http://localhost:1337/uploads/Logo_Colored_PNG_529f0a5229.png?updated_at=2022-04-12T08:59:34.540Z"}
+                  alt="ALKYA's Logo"
+                />
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+            <Navbar.Offcanvas
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id="offcanvasNavbarLabel">ALKYA</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Link to="/mainnews">actus</Link>
+                  <Link to="/aboutus">about us</Link>
+                  <Link to="/mainteam">teams</Link>
+                  <Link to="/shop">shop</Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      </div>
+    </>
   )
 }
--- src/pages/mainteam.js
+++ src/pages/mainteam.js
@@ -1,5 +1,4 @@
-import { Component } from "react";
-import { Col, Container, Image, Row } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Footer from "../components/menu/footer";
 import NavBar from "../components/menu/navbar";
