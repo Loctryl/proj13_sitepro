274f69b9332bca0cbd21b2d7220de06bd7012215
Thomas <tdelorme@gaming.tech>
2022-04-21 19:25:01 +0200

    player.js added key to map child && supress console.log

--- src/pages/player.js
+++ src/pages/player.js
@@ -5,14 +5,13 @@ import "./style/player.css";
 import { Link, useLocation } from "react-router-dom";
 import queryString from 'query-string'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faAngleDown, faArrowRight} from "@fortawesome/free-solid-svg-icons"
+import { faAngleDown, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 
 function Player(props) {
 
   const values = queryString.parse(useLocation().search)
   let player = props.players.data
   player = player.filter(players => players.id === parseInt(values.id))
-  console.log(player[0].attributes.logo_old_team.data);
 
   return (
     <>
@@ -50,25 +49,25 @@ function Player(props) {
           <p className="info-player">Jeu  &emsp;{player[0].attributes.team.data.attributes.game}</p>
         </Col>
       </Row>
-        <h2 className="coloring"><FontAwesomeIcon icon={faAngleDown} className="icon-player" />Carrière joueur</h2>
+      <h2 className="coloring"><FontAwesomeIcon icon={faAngleDown} className="icon-player" />Carrière joueur</h2>
       <Row className="carrer">
         <Col xs={12} md={6} className="carrer">
           <h3 className="name-carrer">"{player[0].attributes.nickname}"<img className="img-carrer-nat" src={"http://localhost:1337" + player[0].attributes.nationality_img.data.attributes.url} alt="Alkya's players images"></img></h3>
           <h4 className="parkour">Parcours</h4>
           <div className="inline-flex spacing">
             {player[0].attributes.logo_old_team.data.map((logo, i) =>
-              <>
-              <img className="logo-old-team" src={"http://localhost:1337" + logo.attributes.url} alt="Alkya's players images"></img> 
-              {i !== (player[0].attributes.logo_old_team.data.length)-1 ?
-                <FontAwesomeIcon icon={faArrowRight} className="logo-to" />
-                :void(0)
-              }
-              </>
+              <div key={i}>
+                <img className="logo-old-team" src={"http://localhost:1337" + logo.attributes.url} alt="Alkya's players images"></img>
+                {i !== (player[0].attributes.logo_old_team.data.length) - 1 ?
+                  <FontAwesomeIcon icon={faArrowRight} className="logo-to" />
+                  : void (0)
+                }
+              </div>
             )}
           </div>
         </Col>
         <Col xs={12} md={6}>
-        <img className="big-img-carrer" src={"http://localhost:1337" + player[0].attributes.image.data[1].attributes.url} alt="Alkya's players images"></img> 
+          <img className="big-img-carrer" src={"http://localhost:1337" + player[0].attributes.image.data[1].attributes.url} alt="Alkya's players images"></img>
         </Col>
       </Row>
       <h3 className="parkour social">Ses réseaux</h3>
@@ -82,16 +81,16 @@ function Player(props) {
         <Col xs={12} md={6} lg={3}>
           <Link to={"/"}><img className="social-icon" src={"http://localhost:1337/uploads/2048px_Instagram_icon_1_cc0d704aeb.png"} alt="Alkya's players images"></img></Link>
         </Col>
-        { player[0].attributes.team.data.attributes.game === "League of Legends" ?
+        {player[0].attributes.team.data.attributes.game === "League of Legends" ?
           <Col xs={12} md={6} lg={3}>
             <Link to={"/"}><img className="social-icon" src={"http://localhost:1337/uploads/thumbnail_LOL_14105970c6.png"} alt="Alkya's players images"></img></Link>
           </Col>
-          :player[0].attributes.team.data.attributes.game === "Valorant" ?
-          <Col xs={12} md={6} lg={3}>
-            <Link to={"/"}><img className="social-icon" src={"http://localhost:1337/uploads/thumbnail_Valo_a1c0776115.jpeg"} alt="Alkya's players images"></img></Link>
-          </Col>
-          :void(0)
-        } 
+          : player[0].attributes.team.data.attributes.game === "Valorant" ?
+            <Col xs={12} md={6} lg={3}>
+              <Link to={"/"}><img className="social-icon" src={"http://localhost:1337/uploads/thumbnail_Valo_a1c0776115.jpeg"} alt="Alkya's players images"></img></Link>
+            </Col>
+            : void (0)
+        }
       </Row>
       <Footer />
     </>
